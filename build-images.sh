4e0ff80daaec0f603a11314a518881b7bb42c490
Stephane de Labrusse <stephdl@de-labrusse.fr>
2023-11-27 11:30:20 +0100

    Update build-images.sh script to include SOGo container

--- build-images.sh
+++ build-images.sh
@@ -10,8 +10,62 @@ set -e
 
 # Prepare variables for later use
 images=()
+# ubuntu_version="jammy-20231004"
+# sogo_version="${sogo_version}"
 # The image will be pushed to GitHub container registry
 repobase="${REPOBASE:-ghcr.io/nethserver}"
+
+
+# #Create webtop-webapp container
+# reponame="sogo-binary"
+# container=$(buildah from docker.io/library/ubuntu:${ubuntu_version})
+# buildah run "${container}" /bin/sh <<'EOF'
+# set -e
+# # Install build dependencies and fetch SOGo and SOPE sources
+# # export DEBIAN_FRONTEND=noninteractive && export TZ=UTC && export sogo_version=5.9.0 && apt-get update && apt-get install -y curl wget jq apache2 dos2unix memcached libssl-dev gnustep-base-runtime libgnustep-base-dev gettext-base lsb-release gnupg --no-install-recommends && \
+# # echo "deb [trusted=yes] http://www.axis.cz/linux/debian jammy sogo-v5" > /etc/apt/sources.list.d/sogo.list && \
+# # apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 608F5293A04BE668 && \
+# # apt-get update && apt-get install -y gnustep-make gnustep-base-runtime libgnustep-base-dev pkg-config make gobjc libz-dev zlib1g-dev libpq-dev libmysqlclient-dev libcurl4-openssl-dev libsodium-dev libxml2-dev libssl-dev libldap2-dev libzip-dev mysql-client postgresql-client tmpreaper python3-m2crypto python3-simplejson python3-vobject python3-dateutil postgresql-server-dev-all libmemcached-dev libcurl4-openssl-dev libwbxml2-* tzdata libytnef0 libytnef0-dev --no-install-recommends
+
+# # wget https://github.com/Alinto/sope/archive/refs/tags/SOPE-${sogo_version}.tar.gz
+# # wget https://github.com/Alinto/sogo/archive/refs/tags/SOGo-${sogo_version}.tar.gz
+# # tar -xf SOPE-${sogo_version}.tar.gz
+# # tar -xf SOGo-${sogo_version}.tar.gz 
+# # mv sope-SOPE-${sogo_version} /tmp/SOPE
+# # mv sogo-SOGo-${sogo_version} /tmp/SOGo 
+# # cd /tmp/SOPE && ./configure --with-gnustep --enable-debug --disable-strip && make && make install
+# # cd /tmp/SOGo && ./configure --enable-debug --disable-strip && make && make install
+
+# # # Configure Apache, SOGo, and other dependencies
+# # a2enmod headers proxy proxy_http rewrite ssl && \
+# # echo "/usr/local/lib/sogo" > /etc/ld.so.conf.d/sogo.conf && \
+# # ldconfig && \
+# # groupadd --system sogo && \
+# # useradd --system --gid sogo sogo && \
+# # install -o sogo -g sogo -m 755 -d /var/run/sogo /var/spool/sogo /var/log/sogo && \
+# # mkdir -p /usr/lib/GNUstep/ && \
+# # ln -s /usr/local/lib/GNUstep/SOGo /usr/lib/GNUstep/SOGo && \
+# # ln -s /usr/local/sbin/sogo-tool /usr/sbin/sogo-tool && \
+# # ln -s /usr/local/sbin/sogo-ealarms-notify /usr/sbin/sogo-ealarms-notify && \
+# # ln -s /usr/local/sbin/sogo-slapd-sockd /usr/sbin/sogo-slapd-sockd
+# echo 'deb [signed-by=/etc/apt/keyrings/sogo.gpg] http://packages.sogo.nu/nightly/5/ubuntu/ jammy jammy' > /etc/apt/sources.list.d/sogo.list && \
+#     curl -s 'https://keys.openpgp.org/vks/v1/by-fingerprint/74FFC6D72B925A34B5D356BDF8A27B36A6E2EAE9' | gpg --dearmor | tee /etc/apt/keyrings/sogo.gpg > /dev/null && \
+#     apt-get update && \
+#     apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y --force-yes && \
+#     apt-get install -y --no-install-recommends gettext-base apache2 sogo sope4.9-dbg sope4.9-gdl1-postgresql memcached libssl-dev && \
+#     apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+# # clean and purge orphan deb
+# #apt purge  automake libtool dovecot-dev libxapian-dev libicu-dev build-essential -y
+# apt-get autoremove --purge -y && \
+# apt-get autoclean -y && \
+# rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /root/.cache/
+# EOF
+# #buildah add "${container}" imapsync/ /
+# # Commit the image
+# buildah commit --rm "${container}" "${repobase}/${reponame}"
+
+
+
 # Configure the image name
 reponame="sogo"
 
@@ -39,7 +93,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/jmalloc/echo-server:latest" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.6 docker.io/frederickzh/archlinux-sogo:5.9.0" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
