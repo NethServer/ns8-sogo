5b35672c7b7141378d1234afc02a6151bcf97048
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-01-12 18:11:04 +0100

    Update container authorizations and labels with bind_user_domains

--- build-images.sh
+++ build-images.sh
@@ -38,7 +38,7 @@ buildah add "${container}" imageroot /imageroot
 buildah add "${container}" ui/dist /ui
 # Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
 buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=traefik@node:routeadm mail@any:mailadm" \
+    --label="org.nethserver.authorizations=traefik@node:routeadm mail@any:mailadm cluster:accountconsumer" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.images=docker.io/mariadb:10.11.6 ghcr.io/nethserver/sogo-server:${sogo_version}" \
