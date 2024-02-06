66ebdf9b3c05393636e036b96c67ea7aa756ee7b
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-02-06 11:28:20 +0100

    Add warning notification for misconfigured mail module

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -58,6 +58,16 @@
                 $t("settings.enabled")
               }}</template>
             </NsToggle>
+            <cv-row v-if="mail_server_URL.length === 0">
+              <cv-column>
+                <NsInlineNotification
+                  kind="warning"
+                  :title="$t('settings.mail_module_misconfigured')"
+                  :description="$t('settings.no_available_mail_domain_check_users')"
+                  :showCloseButton="false"
+                />
+              </cv-column>
+            </cv-row>
             <NsComboBox
               v-model.trim="mail_server"
               :autoFilter="true"
@@ -367,11 +377,6 @@ export default {
         this.ldap_domain = config.ldap_domain;
         // if mail_server_URL is empty, set default value
         if (this.mail_server_URL.length === 0) {
-          this.mail_server_URL.push({
-            label: this.$t("settings.no_available_mail_domain_check_users"),
-            value: "",
-            name: ""
-          });
           // we want to avoid to save the form, there is no users set in the mail domain
           this.mail_server = "";
         }
