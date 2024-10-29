19a43905ece8d7af7399a39991566e1e1fc613fa
Stephane de Labrusse <stephdl@de-labrusse.fr>
2024-10-29 11:20:09 +0100

    Update SOGo version to 5.11.2

--- build-images.sh
+++ build-images.sh
@@ -15,7 +15,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
 reponame="sogo"
-sogo_version="5.11.0"
+sogo_version="5.11.2"
 
 # Create a new empty container image
 container=$(buildah from scratch)
