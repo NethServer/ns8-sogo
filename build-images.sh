e132a1585f6f3fbdc31f55333bd1d8c9e40a8e0f
Stephane de Labrusse <stephdl@de-labrusse.fr>
2026-04-27 14:09:00 +0200

    Update Sogo version to 5.12.7-1

--- build-images.sh
+++ build-images.sh
@@ -16,7 +16,7 @@ repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
 reponame="sogo"
 # To update: visit https://github.com/Alinto/sogo/tags
-sogo_version="fixReverseProxy"
+sogo_version="5.12.7-1"
 
 # Create a new empty container image
 container=$(buildah from scratch)
