d725a7c9f4fb3ddf9572533c45aaae07fbdea209
Stephane de Labrusse <stephdl@de-labrusse.fr>
2026-07-30 15:53:29 +0200

    fix(ui): ns8 module maintenance (#116)
    * chore(ui): remove unwanted VS Code extensions from devcontainer
    
    * build(ui): tune webpack chunk size and silence sass deprecations
    
    * chore(ui): disable no-console eslint rule
    
    * chore(ui): add format script to package.json
    
    * build: require core >= 3.20.1
    
    * build(ui): update axios
    
    * build(ui): update Vue to 2.7
    
    * build(ui): update ns8-ui-lib to latest 2.x
    
    * style(ui): format code with prettier
    
    * build(ui): bump yarn to 4.18.0

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,12 +1,20 @@
 module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          silenceDeprecations: ["import", "global-builtin", "color-functions", "if-function", "legacy-js-api"],
+        },
+      },
+    },
+  },
   publicPath: "./",
   // needed for newer axios versions
   transpileDependencies: ["axios"],
   configureWebpack: {
     optimization: {
       splitChunks: {
-        minSize: 10000,
-        maxSize: 250000,
+        maxSize: 500000,
       },
     },
   },
